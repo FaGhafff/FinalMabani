dea9832cee14102d9dd42945a5dec6102fd42b22
ali <a.forghani.y79@gmail.com>
2021-01-29 19:48:12 +0330

    getter setter for Master.java Student.java

--- src/models/Master.java
+++ src/models/Master.java
@@ -6,6 +6,14 @@ import java.util.Set;
 public class Master extends Person implements Serializable {
     Set<Class> classes;
 
+    public Set<Class> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(Set<Class> classes) {
+        this.classes = classes;
+    }
+
     @Override
     public String toString() {
         return "Master{" +
--- src/models/Student.java
+++ src/models/Student.java
@@ -6,6 +6,14 @@ import java.util.HashMap;
 public class Student extends Person implements Serializable {
     private HashMap<Lesson,Double> lessons;
 
+    public HashMap<Lesson, Double> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(HashMap<Lesson, Double> lessons) {
+        this.lessons = lessons;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
