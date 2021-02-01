789af5c04c46d718165bd54ddc56698e35012892
ali <a.forghani.y79@gmail.com>
2021-02-01 08:22:00 +0330

    for you

--- src/dataLayer/ClassManager.java
+++ src/dataLayer/ClassManager.java
@@ -1,16 +1,69 @@
 package dataLayer;
 
 import models.Class;
+import models.Student;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ClassManager {
 
 
     public ArrayList<Class> readAll() {
-        return null;
+        try {
+            DataBase dataBase = new DataBase();
+            return dataBase.readClasses();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     public void addStudent(String selectedCode, String username) {
+        try {
+            StudentManager studentManager = new StudentManager();
+            Student student = studentManager.getStudent(username);
+            ArrayList<Class> list = new DataBase().readClasses();
+            list.forEach(aClass -> {
+                if (String.valueOf(aClass.getId()).equals(selectedCode))
+                    aClass.getStudents().add(student);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Class get(String classID) {
+        return readAll().stream().filter(aClass -> String.valueOf(aClass.getId()).equals(classID)).findFirst().orElse(null);
+    }
+
+    //save class
+    public void save(Class aClass) {
+        try {
+            DataBase dataBase = new DataBase();
+            ArrayList<Class> list = dataBase.readClasses();
+            list.remove(aClass);
+            list.add(aClass);
+            dataBase.writeClasses(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateClass(HashMap<Long, Boolean> hashMap) {
+        try {
+            DataBase dataBase = new DataBase();
+            ArrayList<Class> list = dataBase.readClasses();
+            hashMap.forEach((aLong, aBoolean) -> list.forEach(aClass -> {
+                if (aClass.getId() == aLong) {
+                    aClass.setEnable(aBoolean);
+                }
+            }));
+            dataBase.writeClasses(list);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/dataLayer/LessonManager.java
+++ src/dataLayer/LessonManager.java
@@ -4,6 +4,7 @@ import models.Lesson;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LessonManager {
 
@@ -13,6 +14,37 @@ public class LessonManager {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public boolean save(Lesson lesson) {
+        try {
+            DataBase dataBase = new DataBase();
+            ArrayList<Lesson> list = dataBase.readLessons();
+            if (list.contains(lesson))
+                return false;
+            list.add(lesson);
+            dataBase.writeLessons(list);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public void update(HashMap<Long, Boolean> hashMap) {
+        try {
+            DataBase dataBase = new DataBase();
+            ArrayList<Lesson> list = dataBase.readLessons();
+            hashMap.forEach((aLong, aBoolean) -> list.forEach(lesson -> {
+                if (aLong==lesson.getId()){
+                    lesson.setPresented(aBoolean);
+                }
+            }));
+            dataBase.writeLessons(list);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
